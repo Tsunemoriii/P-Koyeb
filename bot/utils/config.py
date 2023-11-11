3c92f1ed40c92da67b33790259e2d63555feed26
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-11-11 19:26:53 +0530

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -23,21 +23,21 @@ MAIN_CHANNEL_4 = os.getenv("MAIN_CHANNEL_4","Channel_No_Four")
 MAIN_CHANNEL_5 = os.getenv("MAIN_CHANNEL_5", "Channel_No_Five")
 
 # shortener api keys
-SHORTENER_API = os.environ.get("SHORTENER_API", "c75a81ba04af2e8f1bb88ffbb70085305d19f618")
+SHORTENER_API = os.environ.get("SHORTENER_API", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
 SHORTENER_API_2 = os.environ.get("SHORTENER_API_2", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
-SHORTENER_API_3 = os.environ.get("SHORTENER_API_3", "c75a81ba04af2e8f1bb88ffbb70085305d19f618")
+SHORTENER_API_3 = os.environ.get("SHORTENER_API_3", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
 SHORTENER_API_4 = os.environ.get("SHORTENER_API_4", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
 SHORTENER_API_5 = os.environ.get("SHORTENER_API_5", "23eb5dd17ac140282cad623f70635f2ba0820f4d")
 
 # shortener api urls
-SHORTENER_WEB = os.environ.get("SHORTENER_WEB", "https://onepagelink.in/api?api={0}&url={1}")
+SHORTENER_WEB = os.environ.get("SHORTENER_WEB", "https://moneykamalo.com/api?api={0}&url={1}")
 SHORTENER_WEB_2 = os.environ.get("SHORTENER_WEB_2", "https://moneykamalo.com/api?api={0}&url={1}")
-SHORTENER_WEB_3 = os.environ.get("SHORTENER_WEB_3", "https://onepagelink.in/api?api={0}&url={1}")
+SHORTENER_WEB_3 = os.environ.get("SHORTENER_WEB_3", "https://moneykamalo.com/api?api={0}&url={1}")
 SHORTENER_WEB_4 = os.environ.get("SHORTENER_WEB_4", "https://moneykamalo.com/api?api={0}&url={1}")
 SHORTENER_WEB_5 = os.environ.get("SHORTENER_WEB_5", "https://paisakamalo.in/api?api={0}&url={1}")
 
 # double short url and api
-DOUBLE_SHORT = False    # write False to turn off
+DOUBLE_SHORT = True    # write False to turn off
 DOUBLE_SHORT_WEB = os.environ.get("DOUBLE_SHORT_WEB", "https://onepagelink.in/api?api={0}&url={1}")
 DOUBLE_SHORT_API = os.environ.get("DOUBLE_SHORT_API", "c75a81ba04af2e8f1bb88ffbb70085305d19f618")
 
