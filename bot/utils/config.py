8d6233ba9bdcaaf5803c979adb6b6529d64af212
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-11-17 13:44:48 +0530

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -11,35 +11,35 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001191707498))
 # Bot tokens
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6054245957:AAHPD4Rrlt05HpdMEQnRVSjx6AIgJJ35oDE")
 BOT_TOKEN_2 = os.getenv("BOT_TOKEN_2", "6051155351:AAFTIzVQEytmfPAFZX7OCH60hkP3hCmoQE4")
-BOT_TOKEN_3 = os.getenv("BOT_TOKEN_3", "6086088634:AAH-OOsdwcbDJ4dFC5B1NsskaR7uCCoLyPc")
-BOT_TOKEN_4 = os.getenv("BOT_TOKEN_4", "6225070483:AAE6dKan-ump5yNVhXbva1OkAJ0UQNXbu7I")
-BOT_TOKEN_5 = os.getenv("BOT_TOKEN_5", "5990317516:AAEg4lkBRp1WgqWT8b3U6pG1QKTUIas15DQ")
+BOT_TOKEN_3 = os.getenv("BOT_TOKEN_3", "6722197684:AAExVaNGi0MO1nzY9fi4Sv0nfNl9cpdPcYw")
+BOT_TOKEN_4 = os.getenv("BOT_TOKEN_4", "6917972682:AAFZZx47GWw60zl1JzZ4ryfSWWHaKuKfIME")
+BOT_TOKEN_5 = os.getenv("BOT_TOKEN_5", "6985172423:AAGpV8817eRrxDjF-NUGfuy3FUnoUWr49xU")
 
 # channel usernames for force subscribe (without @)
 MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "Hanimes_Otaku")
 MAIN_CHANNEL_2 = os.getenv("MAIN_CHANNEL_2", "Hanimes_Otaku")
-MAIN_CHANNEL_3 = os.getenv("MAIN_CHANNEL_3", "Hanimes_Otaku")
-MAIN_CHANNEL_4 = os.getenv("MAIN_CHANNEL_4","Channel_No_Four")
-MAIN_CHANNEL_5 = os.getenv("MAIN_CHANNEL_5", "Channel_No_Five")
+MAIN_CHANNEL_3 = os.getenv("MAIN_CHANNEL_3", "One_Piece_Film_Z_Movie")
+MAIN_CHANNEL_4 = os.getenv("MAIN_CHANNEL_4","One_Piece_Film_Gold_Movie")
+MAIN_CHANNEL_5 = os.getenv("MAIN_CHANNEL_5", "Film_Gold_One_Piece")
 
 # shortener api keys
-SHORTENER_API = os.environ.get("SHORTENER_API", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
-SHORTENER_API_2 = os.environ.get("SHORTENER_API_2", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
-SHORTENER_API_3 = os.environ.get("SHORTENER_API_3", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
-SHORTENER_API_4 = os.environ.get("SHORTENER_API_4", "dcddbca543e209dd3f79cfbae6b1dd52cb3ec595")
-SHORTENER_API_5 = os.environ.get("SHORTENER_API_5", "23eb5dd17ac140282cad623f70635f2ba0820f4d")
+SHORTENER_API = os.environ.get("SHORTENER_API", "60e33b784ffac4382a553666021940b5d777bcd2")
+SHORTENER_API_2 = os.environ.get("SHORTENER_API_2", "60e33b784ffac4382a553666021940b5d777bcd2")
+SHORTENER_API_3 = os.environ.get("SHORTENER_API_3", "60e33b784ffac4382a553666021940b5d777bcd2")
+SHORTENER_API_4 = os.environ.get("SHORTENER_API_4", "60e33b784ffac4382a553666021940b5d777bcd2")
+SHORTENER_API_5 = os.environ.get("SHORTENER_API_5", "60e33b784ffac4382a553666021940b5d777bcd2")
 
 # shortener api urls
-SHORTENER_WEB = os.environ.get("SHORTENER_WEB", "https://moneykamalo.com/api?api={0}&url={1}")
-SHORTENER_WEB_2 = os.environ.get("SHORTENER_WEB_2", "https://moneykamalo.com/api?api={0}&url={1}")
-SHORTENER_WEB_3 = os.environ.get("SHORTENER_WEB_3", "https://moneykamalo.com/api?api={0}&url={1}")
-SHORTENER_WEB_4 = os.environ.get("SHORTENER_WEB_4", "https://moneykamalo.com/api?api={0}&url={1}")
-SHORTENER_WEB_5 = os.environ.get("SHORTENER_WEB_5", "https://paisakamalo.in/api?api={0}&url={1}")
+SHORTENER_WEB = os.environ.get("SHORTENER_WEB", "https://publicearn.com/api?api={0}&url={1}")
+SHORTENER_WEB_2 = os.environ.get("SHORTENER_WEB_2", "https://publicearn.com/api?api={0}&url={1}")
+SHORTENER_WEB_3 = os.environ.get("SHORTENER_WEB_3", "https://publicearn.com/api?api={0}&url={1}")
+SHORTENER_WEB_4 = os.environ.get("SHORTENER_WEB_4", "https://publicearn.com/api?api={0}&url={1}")
+SHORTENER_WEB_5 = os.environ.get("SHORTENER_WEB_5", "https://publicearn.com/api?api={0}&url={1}")
 
 # double short url and api
 DOUBLE_SHORT = True    # write False to turn off
-DOUBLE_SHORT_WEB = os.environ.get("DOUBLE_SHORT_WEB", "https://onepagelink.in/api?api={0}&url={1}")
-DOUBLE_SHORT_API = os.environ.get("DOUBLE_SHORT_API", "c75a81ba04af2e8f1bb88ffbb70085305d19f618")
+DOUBLE_SHORT_WEB = os.environ.get("DOUBLE_SHORT_WEB", "https://link2paisa.com/api?api={0}&url={1}")
+DOUBLE_SHORT_API = os.environ.get("DOUBLE_SHORT_API", "636de537452baa8a215b0813f3dfb1a30ef76278")
 
 # original link
 ORIGINAL_LINK = False    # write True to send original link
