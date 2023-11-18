7c33f6f700dc8980c044730fada9af40301e1753
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-11-18 23:59:33 +0530

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -38,8 +38,8 @@ SHORTENER_WEB_5 = os.environ.get("SHORTENER_WEB_5", "https://paisakamalo.in/api?
 
 # double short url and api
 DOUBLE_SHORT = False    # write False to turn off
-DOUBLE_SHORT_WEB = os.environ.get("DOUBLE_SHORT_WEB", "https://paisakamalo.in/api?api={0}&url={1}")
-DOUBLE_SHORT_API = os.environ.get("DOUBLE_SHORT_API", "73580725087e717aebe1220f1614d8944f208970")
+DOUBLE_SHORT_WEB = os.environ.get("DOUBLE_SHORT_WEB", "https://onepagelink.in/api?api={0}&url={1}")
+DOUBLE_SHORT_API = os.environ.get("DOUBLE_SHORT_API", "c75a81ba04af2e8f1bb88ffbb70085305d19f618")
 
 # original link
 ORIGINAL_LINK = False    # write True to send original link
